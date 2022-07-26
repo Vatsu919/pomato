c0297f1a88dc6a4aeb23d0b9fb9d747b83106302
Santosh <santoshpinisetty216@gmail.com>
2022-07-26 23:49:43 +0530

    login layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import CustomerAuthForm from './components/CustomerAuthForm';
+import Loginform from './components/Loginform';
 
 
 function App() {
   return (
     <>
-       <CustomerAuthForm />
+       <Loginform />
     </>
   );
 }
