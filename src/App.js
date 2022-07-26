7f473c945275b4d38fd4533e6aa2f0cc1007f4f3
Vatshal Patel <vatsunp11@gmail.com>
2022-07-26 13:12:28 +0530

    Added halo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <h1>Halooo</h1>
       </header>
     </div>
   );
