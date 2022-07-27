56dfbf91765df33f5aa22eefd8a9bdc23ee2afa6
Santosh <santoshpinisetty216@gmail.com>
2022-07-27 11:41:23 +0530

    no new changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CustomerAuthForm from './components/CustomerAuthForm';
-<<<<<<< HEAD
-import Loginform from './components/Loginform';
-=======
 import { Route, Routes} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 import ManagerAuthForm from './components/ManagerAuthForm/ManagerAuthForm';
 import Home from './components/Home/Home';
->>>>>>> main
 
 
 function App() {
@@ -16,9 +12,6 @@ function App() {
   console.log(user);
   return (
     <>
-<<<<<<< HEAD
-       <Loginform />
-=======
        
         
         <Routes>
@@ -27,7 +20,6 @@ function App() {
           <Route path='/restaurantManagerAuth' element={<ManagerAuthForm />} />
         </Routes>
       
->>>>>>> main
     </>
   );
 }
